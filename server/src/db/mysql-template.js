f3ee9d4053e3009be474628f9c719b62ba2f8dac
seankim <seanistar@gmail.com>
2018-08-15 03:21:10 +0900

    mysql update

--- server/src/db/mysql-template.js
+++ server/src/db/mysql-template.js
@@ -1,15 +1,21 @@
-let mysql = require('mysql');
+const mysql = require('mysql');
 
-const connector =  mysql.createPool({
-  connectionLimit : 10,
-  host: '',
-  user: '',
-  password: '',
-  database: 'medium'
-});
+const connector = process.env.NODE_ENV === 'production' ?
+  mysql.createConnection({
+    host: '',
+    user: '',
+    password: '',
+    database: ''
+  }) :
+  mysql.createPool({
+    connectionLimit : 10,
+    host: '',
+    user: '',
+    password: '',
+    database: ''
+  })
 
-setInterval(function () {
-  connector.query('SELECT 1');
-}, 5000);
+process.env.NODE_ENV === 'production' ? setInterval(() => { connector.query('SELECT 1')}, 5000) : null;
 
+// console.log(connector)
 module.exports = connector;
